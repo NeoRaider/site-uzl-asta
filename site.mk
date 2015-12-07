2b60b74ecdb026596ce92a74091cd7e1cadb9b19
Matthias Schiffer <mschiffer@universe-factory.net>
2015-12-07 13:52:15 +0100

    Gremien-WLAN

--- site.mk
+++ site.mk
@@ -1,24 +1,8 @@
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-14 \
-	gluon-alfred \
-	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-contact-info \
-	gluon-config-mode-core \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-ebtables-segment-mld \
-	gluon-web-admin \
-	gluon-web-autoupdater \
-	gluon-web-mesh-vpn-fastd \
-	gluon-web-network \
-	gluon-web-wifi-config \
-	gluon-mesh-vpn-fastd \
+	gluon-mesh-batman-adv-15 \
+	gluon-authorized-keys \
+	gluon-lock-password \
 	gluon-radvd \
-	gluon-respondd \
 	gluon-setup-mode \
 	gluon-status-page \
 	iwinfo \
@@ -35,4 +19,4 @@ GLUON_PRIORITY ?= 0
 GLUON_REGION ?= eu
 GLUON_LANGS ?= en de
 
-GLUON_ATH10K_MESH=ibss
+GLUON_ATH10K_MESH=11s
